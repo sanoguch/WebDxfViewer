4392d35183f80351a0f2ae9a27347fd3df0b339b
Satoshi Noguchi <sanoguch@gmail.com>
2022-06-01 23:51:04 +0900

    Format source code by Prettier.

--- src/ts/blueprint.ts
+++ src/ts/blueprint.ts
@@ -1,16 +1,16 @@
-import { Helper } from 'dxf'
+import { Helper } from 'dxf';
 
 export class Blueprint {
-    public readDxfFile(dxfContents: string): void {
-        this.dxfHelper = new Helper(dxfContents);
-        // var polylines = this.dxfHelper.toPolylines();
-        // console.log(polylines);
-        // var svgContainer = document.querySelector('#svg');
-        // if (svgContainer) {
-        //     //console.log('parsed:', helper.parsed);
-        //     svgContainer.innerHTML = helper.toSVG();
-        // }
-    }
+  public readDxfFile(dxfContents: string): void {
+    this.dxfHelper = new Helper(dxfContents);
+    // var polylines = this.dxfHelper.toPolylines();
+    // console.log(polylines);
+    // var svgContainer = document.querySelector('#svg');
+    // if (svgContainer) {
+    //     //console.log('parsed:', helper.parsed);
+    //     svgContainer.innerHTML = helper.toSVG();
+    // }
+  }
 
-    private dxfHelper: Helper | null = null;
+  private dxfHelper: Helper | null = null;
 }
--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,52 +1,51 @@
-import { Plotter } from './plotter'
-import { Blueprint } from './blueprint'
+import { Plotter } from './plotter';
+import { Blueprint } from './blueprint';
 
 class Main {
-
-    public initPlotter(): void {
-        const canvas: HTMLCanvasElement = document.querySelector('#canvas') as HTMLCanvasElement;
-        if (canvas) {
-            const width: number = 640;
-            const height: number = 480;
-            this.plotter = new Plotter(canvas, width, height, -width/2.0, -height/2.0, width/2.0, height/2.0);
-        }
+  public initPlotter(): void {
+    const canvas: HTMLCanvasElement = document.querySelector('#canvas') as HTMLCanvasElement;
+    if (canvas) {
+      const width = 640;
+      const height = 480;
+      this.plotter = new Plotter(canvas, width, height, -width / 2.0, -height / 2.0, width / 2.0, height / 2.0);
     }
+  }
 
-    public init(): void {
-        var file = document.querySelector<HTMLInputElement>('#getfile');
-        if (file) {
-            file.onchange = (event: Event) => this.readDxfFile(event)
-        }
+  public init(): void {
+    const file = document.querySelector<HTMLInputElement>('#getfile');
+    if (file) {
+      file.onchange = (event: Event) => this.readDxfFile(event);
     }
+  }
 
-    private readDxfFile(event: Event): void {
-        var file = event.target as HTMLInputElement;
-        if (file) {
-            var fileList = file.files;
-            var reader = new FileReader();
-            if (fileList && fileList.length > 0) {
-                reader.readAsBinaryString(fileList[0])
-                reader.onload = (event: Event) => this.onLoadDxfFile(event)
-            }
-        }
+  private readDxfFile(event: Event): void {
+    const file = event.target as HTMLInputElement;
+    if (file) {
+      const fileList = file.files;
+      const reader = new FileReader();
+      if (fileList && fileList.length > 0) {
+        reader.readAsBinaryString(fileList[0]);
+        reader.onload = (event: Event) => this.onLoadDxfFile(event);
+      }
     }
+  }
 
-    private onLoadDxfFile(event: Event): void {
-        var reader = event.target as FileReader
-        if (reader) {
-            if (reader.readyState === 2) {
-                var dxfContents = reader.result as string;
-                this.blueprint.readDxfFile(dxfContents);
+  private onLoadDxfFile(event: Event): void {
+    const reader = event.target as FileReader;
+    if (reader) {
+      if (reader.readyState === 2) {
+        const dxfContents = reader.result as string;
+        this.blueprint.readDxfFile(dxfContents);
 
-                if (this.plotter) {
-                    this.plotter.render()
-                }
-            }
+        if (this.plotter) {
+          this.plotter.render();
         }
+      }
     }
+  }
 
-    private plotter: Plotter | null = null;
-    private blueprint: Blueprint = new Blueprint();
+  private plotter: Plotter | null = null;
+  private blueprint: Blueprint = new Blueprint();
 }
 
 const main: Main = new Main();
